85d3083c0b9c5359ae2f10cb0d9c2e2ce71249da
David Skrundz <david.skrundz@gmail.com>
2015-09-09 13:37:50 -0600

    Some stuff was done...

--- BuzzerApp/app/src/main/java/ca/skrundz/buzzerapp/SinglePlayerActivity.java
+++ BuzzerApp/app/src/main/java/ca/skrundz/buzzerapp/SinglePlayerActivity.java
@@ -1,49 +1,51 @@
 package ca.skrundz.buzzerapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SinglePlayerActivity extends AppCompatActivity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.activity_main);
+		this.setContentView(R.layout.activity_single_player);
 	}
 
-	public void onPlayAlone(View button) {
-		Intent intent = new Intent(this, SinglePlayerActivity.class);
-		this.startActivity(intent);
-	}
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+//		return super.onTouchEvent(event);
 
-	public void onPlayWithFriends(View button) {
-		Intent intent = new Intent(this, MultiplayerActivity.class);
-		this.startActivity(intent);
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			Log.i("", "Tap");
+		}
+
+		return true;
 	}
 
 	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
+		getMenuInflater().inflate(R.menu.menu_single_player, menu);
 		return true;
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-
+		
 		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			return true;
 		}
-
+		
 		return super.onOptionsItemSelected(item);
 	}
 	*/
