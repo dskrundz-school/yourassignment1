fb3c8254f618c83bfd1672a0ef1b3567f5f0474c
David Skrundz <david.skrundz@gmail.com>
2015-09-11 07:24:57 -0600

    Some more progress

--- BuzzerApp/app/src/main/java/ca/skrundz/buzzerapp/MultiplayerGameActivity.java
+++ BuzzerApp/app/src/main/java/ca/skrundz/buzzerapp/MultiplayerGameActivity.java
@@ -1,26 +1,17 @@
 package ca.skrundz.buzzerapp;
 
 import android.content.Intent;
-import android.graphics.Point;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
-import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MultiplayerGameActivity extends AppCompatActivity {
-	private LinearLayout topLayout = null;
-	private LinearLayout bottomLayout = null;
-
 	private ImageView player1ImageView = null;
 	private ImageView player2ImageView = null;
 	private ImageView player3ImageView = null;
@@ -38,13 +29,10 @@ public class MultiplayerGameActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_multiplayer_game);
 
-		this.topLayout = (LinearLayout)this.findViewById(R.id.topLayout);
-		this.bottomLayout = (LinearLayout)this.findViewById(R.id.bottomLayout);
-
-		this.player1ImageView = (ImageView)this.findViewById(R.id.player1Image);
-		this.player2ImageView = (ImageView)this.findViewById(R.id.player2Image);
-		this.player3ImageView = (ImageView)this.findViewById(R.id.player3Image);
-		this.player4ImageView = (ImageView)this.findViewById(R.id.player4Image);
+		this.player1ImageView = (ImageView) this.findViewById(R.id.player1Image);
+		this.player2ImageView = (ImageView) this.findViewById(R.id.player2Image);
+		this.player3ImageView = (ImageView) this.findViewById(R.id.player3Image);
+		this.player4ImageView = (ImageView) this.findViewById(R.id.player4Image);
 
 		Intent callingIntent = this.getIntent();
 		this.playerCount = callingIntent.getIntExtra(this.getString(R.string.playerCount), 0);
@@ -74,28 +62,25 @@ public class MultiplayerGameActivity extends AppCompatActivity {
 
 			// TODO: Get the frame of the buttons and hit test the touch location
 
-			Log.i("", String.format("touch: %.2f %.2f", x, y));
-
+			if (this.playersHit.size() == 1) {
+				this.timingHandler.postDelayed(this.expireGameRunnable, 1000);
+			} else if (this.playersHit.size() == this.playerCount) {
+				this.endGame();
+			}
 			return true;
 		}
 		return false;
 	}
 
-	public void onRestartGame(View button) {
-		this.startGame();
-	}
-
 	private void startGame() {
 		this.gameRunning = true;
-
-		this.topLayout.setVisibility(View.VISIBLE);
-		this.bottomLayout.setVisibility(View.VISIBLE);
 	}
 
 	private void endGame() {
 		this.gameRunning = false;
 
-		this.topLayout.setVisibility(View.GONE);
-		this.bottomLayout.setVisibility(View.GONE);
+		this.timingHandler.removeCallbacksAndMessages(null);
+
+		// TODO: show an alert with the info and stuff
 	}
 }
